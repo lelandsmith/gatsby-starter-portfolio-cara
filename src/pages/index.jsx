a44b75d3c7af6981db00a6492a0567343a3b4f22
Leland Smith <emailleland@gmail.com>
2019-03-04 16:51:34 -0800

    changing to countdown

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,304 +14,179 @@ import triangle from '../images/triangle.svg';
 import avatar from '../images/avatar.jpg';
 import '../styles/global';
 
-const Divider = styled(ParallaxLayer)`
-  ${tw('absolute w-full h-full')};
-  background: ${props => props.bg};
-  svg {
-    fill: ${props => props.fill};
+
+const zerofill = num => ((num < 10 && num >= 0) ? `0${num}` : num);
+
+const SvgCircle = (props) => {
+  const { className, done, max, radius, stroke, strokeWidth } = props
+  const size = (radius + strokeWidth) * 2
+  const length = Math.ceil(2 * radius * Math.PI)
+  const remainingLength = length - (Math.ceil(2 * radius * Math.PI) * (done / max))
+  return (
+    <svg 
+      className={className}
+      width={size}
+      height={size}
+      viewBox={`0 0 ${size} ${size}`}
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g>
+        <circle 
+          className="circle"
+          r={radius}
+          cx={radius + strokeWidth} 
+          cy={radius + strokeWidth} 
+          stroke={stroke}
+          strokeDasharray={length}
+          strokeDashoffset={remainingLength}
+          strokeLinecap="round"
+          strokeWidth={strokeWidth}
+          fill="none" 
+        />
+        <circle 
+          className="circle--bg"
+          r={radius} 
+          cx={radius + strokeWidth}
+          cy={radius + strokeWidth} 
+          stroke="rgba(0, 0, 0, .1)"
+          strokeLinecap="round"
+          strokeWidth={strokeWidth} 
+          fill="none" 
+        />
+      </g>
+    </svg>
+  )
+}
+
+SvgCircle.defaultProps = {
+  done: 0,
+  max: 24,
+  radius: 72,
+  stroke: '#e91e63',
+  strokeWidth: 8,
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }
   }
-  clip-path: ${props => props.clipPath};
-`;
-
-const DividerMiddle = styled(Divider)`
-  clip-path: polygon(0 15%, 100% 25%, 100% 85%, 0 75%);
-`;
-
-const Content = styled(ParallaxLayer)`
-  ${tw('p-6 md:p-12 lg:p-24 justify-center items-center flex z-50')};
-`;
-
-const Hero = styled.div`
-  ${tw('w-full xl:w-2/3')};
-`;
-
-const Inner = styled.div`
-  ${tw('w-full xxl:w-2/3 text-center lg:text-left')};
-`;
-
-const BigTitle = styled.h1`
-  ${tw('text-5xl lg:text-6xl font-serif text-white mb-6 tracking-wide')};
-  text-shadow: 0 5px 35px rgba(255, 255, 255, 0.15);
-`;
-
-const Title = styled.h1`
-  ${tw('text-4xl lg:text-4xl font-serif text-white mb-8 tracking-wide relative inline-block')};
-  text-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-  &:before {
-    content: '';
-    width: 40px;
-    height: 40px;
-    background: url(${triangle});
-    position: absolute;
-    background-size: 40px;
-    animation: ${rotate} 4s linear infinite;
-    left: -60px;
-    top: 5px;
+  componentWillMount() {
+    this.getTimeUntil(this.props.deadline)
   }
-`;
-
-const Subtitle = styled.p`
-  ${tw('text-2xl lg:text-4xl font-sans text-white mt-8 xxl:w-3/4')};
-  text-shadow: 0 2px 15px rgba(0, 0, 0, 0.2);
-`;
-
-const ProjectsWrapper = styled.div`
-  ${tw('flex flex-wrap justify-between mt-8')};
-  display: grid;
-  grid-gap: 4rem;
-  grid-template-columns: repeat(2, 1fr);
-  @media (max-width: 1200px) {
-    grid-gap: 3rem;
+  componentDidMount() {
+    this.timerId = setInterval(() => this.getTimeUntil(this.props.deadline), 1000)
   }
-  @media (max-width: 900px) {
-    grid-template-columns: 1fr;
-    grid-gap: 2rem;
+  componentWillUnmount() {
+    clearInterval(this.timerId)
   }
-`;
+  getTimeUntil(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date())
+    const seconds = Math.floor(time / 1000 % 60)
+    const minutes = Math.floor(time / 1000 / 60 % 60)
+    const hours = Math.floor(time / (1000 * 60 * 60) % 24)
+    const days = Math.floor(time / (1000 * 60 * 60 * 24))
 
-const WaveWrapper = styled.div`
-  ${tw('absolute pin-b w-full')};
-  transform: matrix(1, 0, 0, -1, 0, 0);
-`;
-
-const InnerWave = styled.div`
-  ${tw('relative h-full')};
-  svg {
-    width: 100%;
-    height: 40vh;
+    this.setState({ days, hours, minutes, seconds })
   }
-`;
-
-const AboutHero = styled.div`
-  ${tw('flex flex-col lg:flex-row items-center mt-8')};
-`;
-
-const Avatar = styled.img`
-  ${tw('rounded-full w-32 xl:w-48 shadow-lg h-auto')};
-`;
-
-const AboutSub = styled.span`
-  ${tw('text-white pt-12 lg:pt-0 lg:pl-12 text-2xl lg:text-3xl xl:text-4xl')};
-`;
-
-const AboutDesc = styled.p`
-  ${tw('text-grey-light text-lg md:text-xl lg:text-2xl font-sans pt-6 md:pt-12 text-justify')};
-`;
-
-const ContactText = styled.p`
-  ${tw('text-grey-light font-sans text-xl md:text-2xl lg:text-3xl')};
-  a {
-    color: #e07628;
-    text-decoration: none;
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__display">
+          <SvgCircle className="clock__circle" max={365} done={this.state.days} />
+          <div className="clock__text clock__text--days">
+            <span className="clock__amount">{zerofill(this.state.days)}</span>
+            <span className="clock__unit">days</span>
+          </div>
+        </div>
+        <div className="clock__display">
+          <SvgCircle max={24} done={this.state.hours} />
+          <div className="clock__text clock__text--hours">
+            <span className="clock__amount">{zerofill(this.state.hours)}</span>
+            <span className="clock__unit">hours</span>
+          </div>
+        </div>
+        <div className="clock__display">
+          <SvgCircle max={60} done={this.state.minutes} />
+          <div className="clock__text clock__text--minutes">
+            <span className="clock__amount">{zerofill(this.state.minutes)}</span>
+            <span className="clock__unit">minutes</span>
+          </div>
+        </div>
+        <div className="clock__display">
+          <SvgCircle max={60} done={this.state.seconds} />
+          <div className="clock__text clock__text--seconds">
+            <span className="clock__amount">{zerofill(this.state.seconds)}</span>
+            <span className="clock__unit">seconds</span>
+          </div>
+        </div>
+      </div>
+    )
   }
-`;
+}
 
-const Footer = styled.footer`
-  ${tw('text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg')};
-  a {
-    color: #e07628;
-    text-decoration: none;
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      deadline: '2019-3-8',
+      error: undefined,
+      newDeadline: undefined,
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
-`;
+  handleChange(e) {
+    this.setState({ newDeadline: e.target.value.trim() })
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    if (isNaN(Date.parse(this.state.newDeadline))) {
+      this.setState({
+        error: "That doesn't seem to be a valid date",
+      })
+    } else if (Date.parse(this.state.newDeadline) < new Date()) {
+      this.setState({ error: 'This date is in the past' })
+    } else {
+      this.setState({
+        deadline: this.state.newDeadline,
+        newDeadline: undefined,
+        error: undefined,
+      })
+    }
+    
+    this.inputRef.value = ''
+  }
+  formatDate() {
+    return new Date(Date.parse(this.state.deadline)).toDateString()
+  }
+  render() {
+    return(
+      <div className="app">
+        <h1 className="app__title">3 Day Mental Diet <br /> {this.formatDate()}</h1>
+        <Clock deadline={this.state.deadline} />
+        {/* <form className="form" onSubmit={this.handleSubmit}>
+          <input 
+            className="form__field"
+            type="text" 
+            placeholder="Set new deadline" 
+            onChange={this.handleChange}
+            ref={(node) => { this.inputRef = node }}
+           />
+          <button className="btn" type="submit">Set Date</button>
+        </form> */}
+        {
+          this.state.error &&
+            <div className="message message--error">{this.state.error}</div>
+        }
+      </div>
+    )
+  }
+}
 
-const Index = () => (
-  <React.Fragment>
-    <SEO />
-    <Parallax pages={5}>
-      <Divider speed={0.2} offset={0}>
-        <UpDown>
-          <SVG icon="triangle" className={hidden} width={48} stroke={colors.orange} left="10%" top="20%" />
-          <SVG icon="hexa" width={48} stroke={colors.red} left="60%" top="70%" />
-          <SVG icon="box" width={6} fill={colors['grey-darker']} left="60%" top="15%" />
-        </UpDown>
-        <UpDownWide>
-          <SVG icon="arrowUp" className={hidden} width={16} fill={colors['blue-dark']} left="80%" top="10%" />
-          <SVG icon="triangle" width={12} stroke={colors.white} left="90%" top="50%" />
-          <SVG icon="circle" width={16} fill={colors['grey-darker']} left="70%" top="90%" />
-          <SVG icon="triangle" width={16} stroke={colors['grey-darkest']} left="30%" top="65%" />
-          <SVG icon="circle" width={6} fill={colors['grey-darkest']} left="75%" top="10%" />
-          <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="45%" top="10%" />
-        </UpDownWide>
-        <SVG icon="circle" className={hidden} width={24} fill={colors['grey-darker']} left="5%" top="70%" />
-        <SVG icon="circle" width={6} fill={colors['grey-darkest']} left="4%" top="20%" />
-        <SVG icon="circle" width={12} fill={colors['grey-darkest']} left="50%" top="60%" />
-        <SVG icon="upDown" width={8} fill={colors['grey-darkest']} left="95%" top="90%" />
-        <SVG icon="upDown" className={hidden} width={24} fill={colors['grey-darker']} left="40%" top="80%" />
-        <SVG icon="triangle" width={8} stroke={colors['grey-darker']} left="25%" top="5%" />
-        <SVG icon="circle" width={64} fill={colors.green} left="95%" top="5%" />
-        <SVG icon="box" className={hidden} width={64} fill={colors.purple} left="5%" top="90%" />
-        <SVG icon="box" width={6} fill={colors['grey-darkest']} left="10%" top="10%" />
-        <SVG icon="box" width={12} fill={colors['grey-darkest']} left="40%" top="30%" />
-        <SVG icon="hexa" width={16} stroke={colors['grey-darker']} left="10%" top="50%" />
-        <SVG icon="hexa" width={8} stroke={colors['grey-darker']} left="80%" top="70%" />
-      </Divider>
-      <Content speed={0.4} offset={0}>
-        <Hero>
-          <BigTitle>
-            Hello, <br /> I'm John Doe.
-          </BigTitle>
-          <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
-        </Hero>
-      </Content>
-      <DividerMiddle
-        bg="linear-gradient(to right, SlateBlue 0%, DeepSkyBlue 100%)"
-        speed={-0.2}
-        offset={1.1}
-        factor={2}
-      />
-      <Content speed={0.4} offset={1.2} factor={2}>
-        <Inner>
-          <Title>Projects</Title>
-          <ProjectsWrapper>
-            <ProjectCard
-              title="Freiheit"
-              link="https://www.behance.net/gallery/58937147/Freiheit"
-              bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
-            >
-              This project is my entry to Adobe's #ChallengeYourPerspective contest.
-            </ProjectCard>
-            <ProjectCard
-              title="Harry Potter"
-              link="https://www.behance.net/gallery/52915793/Harry-Potter"
-              bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
-            >
-              I entered the DOCMA 2017 award with this Harry Potter inspired image.
-            </ProjectCard>
-            <ProjectCard
-              title="Tomb Raider"
-              link="https://www.behance.net/gallery/43907099/Tomb-Raider"
-              bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
-            >
-              Recreation of a Tomb Raider Wallpaper (Fan Art)
-            </ProjectCard>
-            <ProjectCard
-              title="Eagle"
-              link="https://www.behance.net/gallery/38068151/Eagle"
-              bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
-            >
-              A fantasy image manipulation relocating the habitat of wild animals.
-            </ProjectCard>
-          </ProjectsWrapper>
-        </Inner>
-      </Content>
-      <Divider speed={0.1} offset={1} factor={2}>
-        <UpDown>
-          <SVG icon="box" width={6} fill={colors.white} left="85%" top="75%" />
-          <SVG icon="upDown" width={8} fill={colors.teal} left="70%" top="20%" />
-          <SVG icon="triangle" width={8} stroke={colors.orange} left="25%" top="5%" />
-          <SVG icon="circle" className={hidden} width={24} fill={colors.white} left="17%" top="60%" />
-        </UpDown>
-        <UpDownWide>
-          <SVG icon="arrowUp" className={hidden} width={16} fill={colors.green} left="20%" top="90%" />
-          <SVG icon="triangle" width={12} stroke={colors.white} left="90%" top="30%" />
-          <SVG icon="circle" width={16} fill={colors.yellow} left="70%" top="90%" />
-          <SVG icon="triangle" className={hidden} width={16} stroke={colors.teal} left="18%" top="75%" />
-          <SVG icon="circle" width={6} fill={colors.white} left="75%" top="10%" />
-          <SVG icon="upDown" className={hidden} width={8} fill={colors.green} left="45%" top="10%" />
-        </UpDownWide>
-        <SVG icon="circle" width={6} fill={colors.white} left="4%" top="20%" />
-        <SVG icon="circle" width={12} fill={colors.pink} left="80%" top="60%" />
-        <SVG icon="box" width={6} fill={colors.orange} left="10%" top="10%" />
-        <SVG icon="box" width={12} fill={colors.yellow} left="29%" top="26%" />
-        <SVG icon="hexa" width={16} stroke={colors.red} left="75%" top="30%" />
-        <SVG icon="hexa" width={8} stroke={colors.yellow} left="80%" top="70%" />
-      </Divider>
-      <Divider bg="#23262b" clipPath="polygon(0 16%, 100% 4%, 100% 82%, 0 94%)" speed={0.2} offset={3} />
-      <Divider speed={0.1} offset={3}>
-        <UpDown>
-          <SVG icon="box" className={hidden} width={6} fill={colors.blue} left="50%" top="75%" />
-          <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="70%" top="20%" />
-          <SVG icon="triangle" width={8} stroke={colors['grey-darkest']} left="25%" top="5%" />
-          <SVG icon="upDown" className={hidden} width={24} fill={colors.orange} left="80%" top="80%" />
-        </UpDown>
-        <UpDownWide>
-          <SVG icon="arrowUp" className={hidden} width={16} fill={colors.purple} left="5%" top="80%" />
-          <SVG icon="triangle" width={12} stroke={colors.white} left="95%" top="50%" />
-          <SVG icon="circle" width={6} fill={colors.white} left="85%" top="15%" />
-          <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="45%" top="10%" />
-        </UpDownWide>
-        <SVG icon="circle" width={6} fill={colors.white} left="4%" top="20%" />
-        <SVG icon="circle" width={12} fill={colors['grey-darkest']} left="70%" top="60%" />
-        <SVG icon="box" width={6} fill={colors.orange} left="10%" top="10%" />
-        <SVG icon="box" width={12} fill={colors['grey-darkest']} left="20%" top="30%" />
-        <SVG icon="hexa" width={8} stroke={colors['grey-darkest']} left="80%" top="70%" />
-      </Divider>
-      <Content speed={0.4} offset={3}>
-        <Inner>
-          <Title>About</Title>
-          <AboutHero>
-            <Avatar src={avatar} alt="John Doe" />
-            <AboutSub>
-              The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-              Emoji into my speech to better express myself. Winky face.
-            </AboutSub>
-          </AboutHero>
-          <AboutDesc>
-            You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-            every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-            make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The
-            kids want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that
-            all right?
-          </AboutDesc>
-        </Inner>
-      </Content>
-      <Divider fill="#23262b" speed={0.2} offset={4}>
-        <WaveWrapper>
-          <InnerWave>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 338.05" preserveAspectRatio="none">
-              <path className={waveAnimation}>
-                <animate
-                  attributeName="d"
-                  values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-                  repeatCount="indefinite"
-                  dur="30s"
-                />
-              </path>
-            </svg>
-          </InnerWave>
-        </WaveWrapper>
-      </Divider>
-      <Content speed={0.4} offset={4}>
-        <Inner>
-          <Title>Get in touch</Title>
-          <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
-          </ContactText>
-        </Inner>
-        <Footer>
-          &copy; 2018 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>.
-        </Footer>
-      </Content>
-      <Divider speed={0.1} offset={4}>
-        <UpDown>
-          <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="70%" top="20%" />
-          <SVG icon="triangle" width={8} stroke={colors['grey-darkest']} left="25%" top="5%" />
-        </UpDown>
-        <UpDownWide>
-          <SVG icon="triangle" width={12} stroke={colors.white} left="95%" top="50%" />
-          <SVG icon="circle" width={6} fill={colors.white} left="85%" top="15%" />
-          <SVG icon="upDown" className={hidden} width={8} fill={colors['grey-darkest']} left="45%" top="10%" />
-        </UpDownWide>
-        <SVG icon="circle" width={6} fill={colors.white} left="4%" top="20%" />
-        <SVG icon="circle" width={12} fill={colors['grey-darkest']} left="70%" top="60%" />
-        <SVG icon="box" width={12} fill={colors['grey-darkest']} left="20%" top="30%" />
-        <SVG icon="hexa" width={8} stroke={colors['grey-darkest']} left="80%" top="70%" />
-      </Divider>
-    </Parallax>
-  </React.Fragment>
-);
-
-export default Index;
+export default App;
--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -2,23 +2,132 @@
 import { injectGlobal } from 'emotion';
 
 injectGlobal`
-  *, *:before, *:after {
-    box-sizing: inherit;
-  }
-  html {
-    text-rendering: optimizeLegibility;
-    overflow-x: hidden;
-    box-sizing: border-box;
-    -ms-overflow-style: scrollbar;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    background-color: #161719;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
-  html, body {
-    width: 100%;
-    height: 100%;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: inherit;
+}
+
+html, body, #app {
+  height: 100%;
+}
+
+body {
+  background-attachment: fixed;
+  background-blend-mode: normal;
+  background-color: #cddc39;
+  background-image: linear-gradient(135deg, #009688, rgba(255, 255, 255, 0.2));
+  color: rgba(0, 0, 0, 0.65);
+  font-family: "Nixie One", "Segoe UI", "Helvetica Neue", sans-serif;
+  font-weight: 400;
+  margin: 0;
+}
+
+button,
+input {
+  color: inherit;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+}
+
+::-webkit-input-placeholder {
+  color: rgba(153, 153, 153, 0.65);
+}
+
+:-ms-input-placeholder {
+  color: rgba(153, 153, 153, 0.65);
+}
+
+::-ms-input-placeholder {
+  color: rgba(153, 153, 153, 0.65);
+}
+
+::placeholder {
+  color: rgba(153, 153, 153, 0.65);
+}
+
+.app {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  min-height: 100%;
+}
+.app__title {
+  font-weight: 400;
+  margin: 1rem auto 2rem;
+  text-align: center;
+  font-size: 60px;
+}
+
+.clock {
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 2rem;
+}
+.clock__display {
+  height: 160px;
+  position: relative;
+  width: 160px;
+}
+.clock__text {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: center;
+  position: absolute;
+  text-align: center;
+  top: 0;
+  width: 100%;
+}
+.clock__amount {
+  font-size: 3.75rem;
+}
+.clock__unit {
+  font-size: 0.75rem;
+}
+
+.circle {
+  -webkit-transform: rotate(-90deg);
+          transform: rotate(-90deg);
+  -webkit-transform-origin: 50% 50%;
+          transform-origin: 50% 50%;
+  transition: stroke-dashoffset 0.4s cubic-bezier(0.4, 0, 0, 1.7);
+}
+
+.form {
+  align-items: center;
+  display: flex;  
+  margin-bottom: 1rem;
+}
+
+.btn,
+.form__field {
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  padding: 1rem 0.5rem;
+}
+
+.form__field {
+  flex-shrink: 1;
+}
+
+.btn {
+  background-color: #e91e63;
+  color: rgba(255, 255, 255, 0.75);
+  cursor: pointer;
+  flex-shrink: 1;
+}
+
+.message {
+  margin: 0 auto 1rem;
+  text-align: center;
+}
+.message--error {
+  background-color: #e91e63;
+  color: rgba(255, 255, 255, 0.6);
+  padding: 1rem;
+}
+
 `;
